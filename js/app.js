b037acefbcfd97476533e8da44800c4e2d1f0233
luiscar2050 <luis3206162549@gmail.com>
2024-12-01 11:05:06 -0600

    segunda funcionalidad del diccionario terminado

--- js/app.js
+++ js/app.js
@@ -39,3 +39,55 @@ function translateWord() {
         resultElement.textContent = `No se encontró la palabra "${wordInput}" en el diccionario.`;
     }
 }
+
+/*----------------------*/
+
+function displayWords() {
+    const selectedCategory = document.querySelector('input[name="category"]:checked').value;
+    console.log("Categoría seleccionada:", selectedCategory);
+    const wordsList = dictionary.categories[selectedCategory];
+
+    if (wordsList && Array.isArray(wordsList)) {
+        const tableResult = document.querySelector('.table-result');
+        tableResult.innerHTML = `<h1>Palabras de ${selectedCategory}</h1>`;
+
+        const ul = document.createElement('ul');
+        wordsList.forEach(word => {
+            const li = document.createElement('li');
+            li.textContent = word.english + " - " + word.spanish + ": " + word.example;
+            ul.appendChild(li);
+        });
+
+        tableResult.appendChild(ul);
+    } else {
+        const tableResult = document.querySelector('.table-result');
+        tableResult.innerHTML = `<h1>Palabras de ${selectedCategory}</h1><p>No se encontraron palabras para esta categoría.</p>`;
+        console.error("No se encontraron palabras para esta categoría.");
+    }
+}
+
+function sortDictionary() {
+    const selectedCategory = document.querySelector('input[name="category"]:checked').value;
+    console.log("Categoría seleccionada para ordenar:", selectedCategory); // Para depuración
+    let wordsList = dictionary.categories[selectedCategory]; // Obtiene las palabras de la categoría seleccionada
+
+    if (wordsList && Array.isArray(wordsList)) {
+        wordsList.sort((a, b) => a.english.localeCompare(b.english)); // Ordena las palabras alfabéticamente por su nombre en inglés
+
+        displayWords();
+    } else {
+        console.error("No se pueden ordenar las palabras porque no hay ninguna categoría seleccionada.");
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayWords();
+
+    const categoryRadios = document.querySelectorAll('input[name="category"]');
+    categoryRadios.forEach(radio => {
+        radio.addEventListener('change', displayWords);
+    });
+
+    const sortButton = document.querySelector('.btn');
+    sortButton.addEventListener('click', sortDictionary);
+});
